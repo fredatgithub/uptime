7dac9ef3c6749faaae68d3652353db5897474558
Freddy Juhel <domaine75@gmail.com>
2014-08-06 11:41:30 +0200

    initial project

---
+++ Uptime/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace Uptime
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("UpTime.exe is a Freeware written by Freddy Juhel in 2013\n");
+
+      //Console.WriteLine(dt.ToString("dd:HH:mm:ss:ff"));
+
+      //string myDate = new DateTime(Environment.TickCount).ToString("dd:HH:mm:ss:ms");
+
+      var ts = TimeSpan.FromMilliseconds(Environment.TickCount);
+
+      int nombreDeTicks = Environment.TickCount; // &Int32.MaxValue;
+
+      Console.WriteLine("System is up since {0:HH:mm:ss} milliseconds\n", Environment.TickCount.ToString(CultureInfo.CurrentCulture));
+      Console.WriteLine("which is {0} day{1} {2} hour{3} {4} minute{5} {6} second{7} {8} millisecond{9}\n",
+        ts.Days, Plural(ts.Days), ts.Hours, Plural(ts.Hours), ts.Minutes, Plural(ts.Minutes), ts.Seconds, Plural(ts.Seconds), ts.Milliseconds, Plural(ts.Milliseconds));
+      Console.WriteLine("Press a key to exit:");
+      Console.ReadKey();
+    }
+
+    private static string Plural(double number)
+    {
+      return number > 1 ? "s" : string.Empty;
+    }
+    private static string Plural(Byte number)
+    {
+      return number > 1 ? "s" : string.Empty;
+    }
+    private static string Plural(SByte number)
+    {
+      return number > 1 ? "s" : string.Empty;
+    }
+    private static string Plural(Int16 number)
+    {
+      return number > 1 ? "s" : string.Empty;
+    }
+    private static string Plural(Int32 number)
+    {
+      return number > 1 ? "s" : string.Empty;
+    }
+    private static string Plural(Int64 number)
+    {
+      return number > 1 ? "s" : string.Empty;
+    }
+    private static string Plural(UInt16 number)
+    {
+      return number > 1 ? "s" : string.Empty;
+    }
+    private static string Plural(UInt32 number)
+    {
+      return number > 1 ? "s" : string.Empty;
+    }
+    private static string Plural(UInt64 number)
+    {
+      return number > 1 ? "s" : string.Empty;
+    }
+    private static string Plural(float number)
+    {
+      return number > 1 ? "s" : string.Empty;
+    }
+    private static string Plural(decimal number)
+    {
+      return number > 1 ? "s" : string.Empty;
+    }
+
+    private static string BuildUpTime(int temps)
+    {
+      StringBuilder result = new StringBuilder();
+      if (HasHours(temps))
+      {
+        result.Append(temps / 216000000);
+      }
+      else
+      {
+        result.Append("00 hour ");
+      }
+
+      if (HasMinutes(temps))
+      {
+        result.Append(temps / 216000000);
+      }
+      else
+      {
+        result.Append("00 minute ");
+      }
+
+      if (HasSeconds(temps))
+      {
+        result.Append(temps / 216000000);
+      }
+      else
+      {
+        result.Append("00 second ");
+      }
+
+      if (HasMilliSeconds(temps))
+      {
+        result.Append(temps / 216000000);
+      }
+      else
+      {
+        result.Append("00 millisecond ");
+      }
+
+      return result.ToString();
+    }
+
+    private static bool HasHours(int temps)
+    {
+      return ((temps / 216000000) > 0);
+    }
+
+    private static bool HasMinutes(int temps)
+    {
+      return ((temps / 3600000) > 0);
+    }
+
+    private static bool HasSeconds(int temps)
+    {
+      return ((temps / 60000) > 0);
+    }
+
+    private static bool HasMilliSeconds(int temps)
+    {
+      return temps > 0;
+    }
+  }
+}
---
+++ Uptime/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("Uptime")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Uptime")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("c6e0577c-0d82-4447-a33d-21bb9f9f3104")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
