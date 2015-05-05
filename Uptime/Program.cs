6dc530da007fa84f3e8300bf08b47aa727b00f51
Freddy Juhel <domaine75@gmail.com>
2015-05-05 18:30:22 +0200

    Add license and display delegate

--- Uptime/Program.cs
+++ Uptime/Program.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+The MIT License(MIT)
+Copyright(c) 2015 Freddy Juhel
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using System;
 using System.Globalization;
 using System.Text;
 
@@ -8,6 +27,7 @@ namespace Uptime
   {
     static void Main()
     {
+      Action<string> display = s => Console.WriteLine();
       Console.WriteLine("UpTime.exe is a Freeware written by Freddy Juhel in 2013\n");
 
       //Console.WriteLine(dt.ToString("dd:HH:mm:ss:ff"));
@@ -29,11 +49,11 @@ namespace Uptime
     {
       return number > 1 ? "s" : string.Empty;
     }
-    private static string Plural(Byte number)
+    private static string Plural(byte number)
     {
       return number > 1 ? "s" : string.Empty;
     }
-    private static string Plural(SByte number)
+    private static string Plural(sbyte number)
     {
       return number > 1 ? "s" : string.Empty;
     }
